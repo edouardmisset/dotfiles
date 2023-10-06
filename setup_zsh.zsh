c2c5a5e7256b003de826b9aeddb2cd2f2ba7142f
Edouard Misset <63284636+edouardmisset@users.noreply.github.com>
2023-10-06 12:26:37 +0200

    prevent superfluous commands & symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,8 +2,23 @@
 
 echo "\n<<<Starting ZSH Setup >>>\n"
 
-echo "Enter superuser (sudo) password to edit the acceptable shells in /etc/shells"
-echo /usr/local/bin/zsh | sudo tee -a /etc/shells >/dev/null
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+  echo '/usr/local/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit the acceptable shells in /etc/shells"
+  echo /usr/local/bin/zsh | sudo tee -a /etc/shells >/dev/null
+fi
 
-echo "Enter user password to login change shell"
-chsh -s /usr/local/bin/zsh
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+  echo '$SHELL is already in /usr/local/bin/zsh'
+else
+  echo "Enter user password to login change shell"
+  chsh -s /usr/local/bin/zsh
+fi
+
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh as the default non-interactive shell"
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
